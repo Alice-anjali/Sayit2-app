0d7a8260d94f4c24631edd6642c8cd41b6dbdf39
Alice-anjali <alice.anjali1995@gmail.com>
2017-06-26 23:44:07 +0530

    minor changes

--- routes/users.js
+++ routes/users.js
@@ -4,37 +4,53 @@ var path = require('path');
 var User = require('../models/user');
 
 router.post('/signup', function(req, res, next) {
-  User.findOne({email : req.body.email}, function(err,user){
-  if (err){
-     return handleError(err);
+  User.findOne({email : req.body.email}, function(err,user_email){
+    if (err){
+      return handleError(err);
   }
   else{
-    if (user){
-    console.log("User already present!");
+    if (user_email){
+    console.log("Email ID already in use!");
     }
     else{
-      if(req.body.password1 == req.body.password2){
-        console.log("Creating a new user");
-        var newUser = new User({
-          email : req.body.email,
-          name : req.body.name,
-          username : req.body.username,
-          password : req.body.password1,
-          review : {msg : "Just checking at 0"}
-        });
-        newUser.save(function(err){
+        User.findOne({username : req.body.username}, function(err,user_name){
           if (err){
-            return handleError(err);
+             return handleError(err);
           }
           else{
-            console.log("New User created");
-            res.redirect('/');
+            if (user_name){
+            console.log("Username already taken!");
+            // res.redirect('/');
+            // res.status(100).json({ error: 'message' });
+            // res.status(500).send({ error: "username already taken" });
+            //  Materialize.toast('I am a toast!', 4000)
+            }
+            else {
+              if(req.body.password1 == req.body.password2){
+                console.log("Creating a new user");
+                var newUser = new User({
+                  email : req.body.email,
+                  name : req.body.name,
+                  username : req.body.username,
+                  password : req.body.password1,
+                  review : {msg : "Just checking at 0"}
+                });
+                newUser.save(function(err){
+                  if (err){
+                    return handleError(err);
+                  }
+                  else{
+                    console.log("New User created");
+                    res.redirect('/');
+                  }
+                });
+              }
+              else{
+                console.log("password doesnt match");
+              }
+            }
           }
         });
-      }
-      else{
-        console.log("password doesnt match");
-      }
     }
   }
 });
